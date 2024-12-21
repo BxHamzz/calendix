286a0b7c4866f397715c23ce57d6edf93e0c3fb2
LC.Student <lc.student@b1-odimbp02.myfiosgateway.com>
2024-12-21 17:11:13 -0500

    Added a profile page

--- src/app/(site)/dashboard/page.tsx
+++ src/app/(site)/dashboard/page.tsx
@@ -1,13 +1,14 @@
 import DashboardNav from "@/app/components/DashboardNav";
-import Link from "next/link";
 
 export default function DashboardPage() {
-    return (
-        <>
-          <DashboardNav />
-          <div>
-            booked events listed here...
-          </div>
-        </>
+    return(
+        
+       
+        <div>
+            <DashboardNav />
+            Dashboard page
+
+        </div>
     );
+
 }
--- src/app/components/DashboardNav.tsx
+++ src/app/components/DashboardNav.tsx
@@ -3,24 +3,34 @@ import {clsx} from "clsx";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 
-export default function DashboardNav({username}:{username?:string}) {
+export default function DashboardNav() {
   const pathname = usePathname();
   const isEventTypesPage = pathname.includes('event-types');
+  const isBookedEventsPage = pathname.includes('booked-events');
   return (
     <div className="flex gap-4 justify-center">
       <Link
         className={
           clsx(
             'rounded-full px-4 py-2',
-            {
-                'bg-gray-200': isEventTypesPage,
-                'bg-blue-600 text-white': !isEventTypesPage,
-            }
+            pathname === '/dashboard' ? 'bg-blue-600 text-white' : 'bg-gray-200'
           )
         }
         href={'/dashboard'}>
-        Booked events
+        Profile
       </Link>
+     
+        <>
+          <Link
+            className={
+              clsx(
+                'rounded-full px-4 py-2',
+                isBookedEventsPage ? 'bg-blue-600 text-white' : 'bg-gray-200'
+              )
+            }
+            href={'/dashboard/booked-events'}>
+            Booked events
+          </Link>
           <Link
             className={
               clsx(
@@ -31,6 +41,7 @@ export default function DashboardNav({username}:{username?:string}) {
             href={'/dashboard/event-types'}>
             Event types
           </Link>
+        </>
     </div>
   );
 }
